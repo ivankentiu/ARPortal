d5b89bce836d86f596d832ed82a7775cf23150c8
ivantiu <ivankentiu@gmail.com>
2017-09-29 20:47:14 +0800

    portal scn construction

---
+++ AR-Portal/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AR-Portal
+//
+//  Created by Ivan Ken Tiu on 28/09/2017.
+//  Copyright © 2017 FinalShift Inc. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var planeDetected: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
+    }
+}
+
